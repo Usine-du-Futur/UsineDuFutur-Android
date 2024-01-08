a5c6a6d81501a7b4934784ec3172a1c6c1d74928
Marcelo Vogt <marcelovogt43@gmail.com>
2024-01-08 14:44:42 +0100

    Added Song class

---
+++ app/src/main/java/app/v43/usinedufutur/application/circuit/Song.java
@@ -0,0 +1,10 @@
+package app.v43.usinedufutur.application.circuit;
+
+public final class Song {
+    public int id;
+    public String name;
+    public Song(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
